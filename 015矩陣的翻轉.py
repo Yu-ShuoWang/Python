fe9e8224cdc9a4c032799ef496dde9f58eeb753f
Yu-ShuoWang <81060051+Yu-ShuoWang@users.noreply.github.com>
2021-03-25 13:25:19 +0800

    Update 015矩陣的翻轉.py

--- 015矩陣的翻轉.py
+++ 015矩陣的翻轉.py
@@ -4,24 +4,23 @@ Created on Sat Nov 7 16:22:30 2020
 
 @author: YOSO_WANHH
 """
-''' #無法執行
-import sys
+
 mat = [[0] * 100 for i in range(100) ]   #宣告二維矩陣
-for line in sys.stdin:
-    m, n = line.split()
-    m = int(m)
-    n = int(n)
-    for i in range(m):  #輸入陣列
-        list = sys.stdin.readline().split()
-        for j in range(n):
-            mat[i][j] = list[j]
-    for i in range(n):  #輸出順序改變就會達成轉置矩陣
-        for j in range(m):
-            print(mat[j][i]," ",sep="",end="")
-        print()
+
+m, n = input().split()
+m = int(m)
+n = int(n)
+for i in range(m):  #輸入陣列
+    list = input().split()
+    for j in range(n):
+        mat[i][j] = list[j]
+for i in range(n):  #輸出順序改變就會達成轉置矩陣
+    for j in range(m):
+        print(mat[j][i]," ",sep="",end="")
+    print()
+
+       
 '''
-
-
 while True :
     print('输入大小')
     a = input()#横
