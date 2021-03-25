b8ae574837160be615151bbcae3578e6c3649a56
Yu-ShuoWang <81060051+Yu-ShuoWang@users.noreply.github.com>
2021-03-25 13:57:09 +0800

    Update 03兩光法師占卜術.py

--- 03兩光法師占卜術.py
+++ 03兩光法師占卜術.py
@@ -18,9 +18,9 @@ else:
     print("大吉")
 '''
 
-month = int(input("月份: "))
-date  = int(input("日期: "))
-
+month, date = input().split()
+month = int(month)
+date = int(date)
 function = (month*2+date)%3
 if(function==0):
     print("普通")
